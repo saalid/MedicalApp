72e7d770d2928799bbd8b4d6b25b7a84628db539
Amirreza <amirreza.salari.282@gmail.com>
2022-03-19 08:50:39 +0330

    Ready to code

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,5 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
